1360906d2e14b7e7f7a0679ab3f02c13929dde68
Pascal Klesse <54418919+pascal-klesse@users.noreply.github.com>
2023-03-31 21:48:21 +0200

    Update nuxt.config.ts

--- nuxt-base-template/nuxt.config.ts
+++ nuxt-base-template/nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: './src',
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@lenne.tech/nuxt-base'],
+  nuxtBase: {
+    host: 'http://localhost:3001/api'
+  },
   imports: {
     dirs: ['./states', './stores'],
   },
