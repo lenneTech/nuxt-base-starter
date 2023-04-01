48189ea30501b698e2edb7d4cdfc56006964839f
Pascal Klesse <pascal.klesse@lenne.tech>
2023-04-01 10:16:27 +0200

    feat: Set nuxt-base watch prop default to false

--- nuxt-base-template/nuxt.config.ts
+++ nuxt-base-template/nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: './src',
-  modules: ['@nuxtjs/tailwindcss', '@lenne.tech/nuxt-base'],
+  srcDir: "./src",
+  modules: ["@nuxtjs/tailwindcss", "@lenne.tech/nuxt-base"],
   nuxtBase: {
-    host: 'http://localhost:3100/api'
+    host: "http://localhost:3100/api",
+    watch: false,
+    autoImport: true,
+    apollo: {
+      authType: "Bearer",
+      authHeader: "Authorization",
+      tokenStorage: "cookie",
+      proxyCookies: true,
+    },
   },
   imports: {
-    dirs: ['./states', './stores'],
+    dirs: ["./states", "./stores"],
   },
-})
+});
