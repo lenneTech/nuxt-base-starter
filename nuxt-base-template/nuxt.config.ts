0fdc1ee1365bade6157620c307c7efe6080a4f4f
Pascal Klesse <pascal.klesse@lenne.tech>
2023-06-12 20:46:02 +0200

    fix: fix vueuse integration

--- nuxt-base-template/nuxt.config.ts
+++ nuxt-base-template/nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "./src",
-  modules: ["@nuxtjs/tailwindcss", "@lenne.tech/nuxt-base"],
+  modules: ["@nuxtjs/tailwindcss", "@lenne.tech/nuxt-base", "@vueuse/nuxt"],
   nuxtBase: {
-    host: "http://localhost:3100/api",
+    host: "http://localhost:3100/graphql",
     watch: false,
     autoImport: true,
     apollo: {
