c4f284e6983f99340ac8c4f03326cd17f166fcc8
Pascal Klesse <pascal.klesse@lenne.tech>
2023-06-26 22:25:49 +0200

    feat:: Add formkit

--- nuxt-base-template/nuxt.config.ts
+++ nuxt-base-template/nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "./src",
-  modules: ["@nuxtjs/tailwindcss", "@lenne.tech/nuxt-base", "@vueuse/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@lenne.tech/nuxt-base",
+    "@vueuse/nuxt",
+    "@formkit/nuxt",
+  ],
   nuxtBase: {
-    host: "http://localhost:3100/graphql",
+    host: "http://localhost:3000/graphql",
+    schema: "../api/schema.gql",
     watch: false,
     autoImport: true,
     apollo: {
@@ -14,6 +20,6 @@ export default defineNuxtConfig({
     },
   },
   imports: {
-    dirs: ["./states", "./stores"],
+    dirs: ["./states", "./stores", "./forms"],
   },
 });
---
+++ nuxt-base-template/src/middleware/auth.global.ts
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const store = useAuthStore();
+
+  if (to.fullPath.startsWith('/auth/')) return;
+
+  if (!store.token || !store.currentUser) return navigateTo('/auth/login');
+});
